d53579232f061aeaa81cedc1f226ebd09bfb5933
Caleb Andreano <andreanoc@msoe.edu>
2022-03-26 18:41:32 -0500

    poop cock

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPXL8.h>
 
+
+#define NUM_LED 526  
+
+const int BUTTON_BLUE_PIN = 7; // pin for blue button
+const int BUTTON_RED_PIN = 8; // pin for red button
+
+int8_t pins[8] = { 13, 12, 11, 10, SCK, 5, 9, 6 };
+Adafruit_NeoPXL8 leds(NUM_LED, pins, NEO_GRB);
+
+int currentStateRed;
+int currentStateBlue;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  leds.begin();
+  leds.setBrightness(100);
+
+  pinMode(BUTTON_BLUE_PIN, INPUT_PULLUP); // active low button
+  pinMode(BUTTON_RED_PIN, INPUT_PULLUP); // active low button
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  currentStateBlue = digitalRead(BUTTON_BLUE_PIN);
+  currentStateRed = digitalRead(BUTTON_RED_PIN);
+
+  leds.setPixelColor()
+
+
 }
