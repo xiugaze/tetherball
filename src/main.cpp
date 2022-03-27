7d955bf257a08d700f192ea4739a4abcdd6b2ed0
Caleb Andreano <andreanoc@msoe.edu>
2022-03-26 20:42:15 -0500

    test button reverse direction

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPXL8.h>
 
-
 #define NUM_LED 526  
 
 const int BUTTON_BLUE_PIN = 2; // pin for blue button
@@ -10,8 +9,11 @@ const int BUTTON_RED_PIN = 3; // pin for red button
 int8_t pins[8] = { 13, 12, 11, 10, SCK, 5, 9, 6 };
 Adafruit_NeoPXL8 leds(NUM_LED, pins, NEO_GRB);
 
-bool direction = true; 
+bool direction = true;  // true == counterclockwise (away from origin)
+
+int prevPosition = 526;
 int currentPosition = 1;
+int nextPosition =2; 
 
 int currentStateRed;
 int currentStateBlue;
@@ -36,12 +38,19 @@ void loop() {
   }
 
   if(currentPosition == 526 && direction) {
+    nextPosition = 2;
     currentPosition = 1;
+    prevPosition = 526;
   } else if (currentPosition == 1 && !direction) {
+    prevPosition = 1;
     currentPosition = 526;
+    nextPosition = 525;
   } else {
     currentPosition = direction ? currentPosition + 1 : currentPosition - 1;
+    nextPosition = direction ? nextPosition + 1 : nextPosition - 1;
+    prevPosition = direction ? prevPosition + 1 : prevPosition - 1;
   }
+
   leds.setPixelColor(currentPosition - 1, 0x000000);
   leds.setPixelColor(currentPosition, 0xFFFFFF);
   leds.setPixelColor(currentPosition + 1, 0x000000);
